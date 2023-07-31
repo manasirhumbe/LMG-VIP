17e9d986f7c8ddaa5258bd2affc289fa5711ceff
Manasi Humbe <109661219+manasirhumbe@users.noreply.github.com>
2023-07-31 17:43:06 +0530

    initial commi

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return (
+            <>
+
+            <div className="text-center new">
+              
+            <p>LGM VIP </p>
+            </div>
+       
+             </>
+    )
+}
